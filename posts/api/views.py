0f4c00afaa7cf65fe8c702229bacf95d1eb4fc27
Evan B <admin@storm-east.com>
2018-06-12 06:02:37 -0600

    Queryset Filter by Q and rest_framework

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,12 +1,37 @@
+from django.db.models import Q
+
+from rest_framework.filters import (SearchFilter, OrderingFilter)
+
 from rest_framework.generics import ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView, CreateAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 
 from posts.models import Post
 from .serializers import PostListSerializer, PostDetailSerializer, PostCreateSerializer
+
+from .permissions import IsOwnerOrReadOnly
 
 
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
+
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'content', 'user__first_name']
+
+    permission_classes = [AllowAny]
+
+    def get_queryset(self, *args, **kwargs):
+        # queryset_list = super(PostListAPIView, self).get_queryset(*args, **kwargs)
+        queryset_list = Post.objects.all()
+        query = self.request.GET.get("q")
+        if query:
+            queryset_list = queryset_list.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(user__first_name__icontains=query) |
+                Q(user__last_name__icontains=query)
+            ).distinct()
+        return queryset_list
 
 
 class PostDetailAPIView(RetrieveAPIView):
@@ -18,7 +43,9 @@ class PostDetailAPIView(RetrieveAPIView):
 
 class PostUpdateAPIView(UpdateAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostListSerializer
+    serializer_class = PostCreateSerializer
+
+    permission_classes = [IsOwnerOrReadOnly]
 
     def perform_update(self, serializer):
         serializer.save(user=self.request.user)
@@ -28,10 +55,14 @@ class PostDeleteAPIView(DestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
 
+    lookup_field = 'slug'
+    permission_classes = [IsOwnerOrReadOnly]
+
 
 class PostCreateAPIView(CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostCreateSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
