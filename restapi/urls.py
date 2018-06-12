493fa24234189b72b701b290495f6c0dc2ee8cae
Evan B <admin@storm-east.com>
2018-06-12 07:48:53 -0600

    Comments API and fixing some minor errors

--- restapi/urls.py
+++ restapi/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     url(r'^posts/', include("posts.urls", namespace="posts")),
     url(r'^api/posts/', include("posts.api.urls", namespace="posts-api")),
     # url(r'^api-auth/', include('rest_framework.urls')),
-    # url(r'^comments/', include('django_comments.urls')),
+    url(r'^api/comments/', include('comments.api.urls', namespace="comments-api")),
 ]
 
 if settings.DEBUG:
