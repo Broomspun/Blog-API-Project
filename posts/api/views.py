f08ee63c830e9b2f5a79618f3bdcd82afdaa7081
Evan B <admin@storm-east.com>
2018-06-12 06:19:44 -0600

    Pagination by rest_framework

--- posts/api/views.py
+++ posts/api/views.py
@@ -9,29 +9,7 @@ from posts.models import Post
 from .serializers import PostListSerializer, PostDetailSerializer, PostCreateSerializer
 
 from .permissions import IsOwnerOrReadOnly
-
-
-class PostListAPIView(ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostListSerializer
-
-    filter_backends = [SearchFilter, OrderingFilter]
-    search_fields = ['title', 'content', 'user__first_name']
-
-    permission_classes = [AllowAny]
-
-    def get_queryset(self, *args, **kwargs):
-        # queryset_list = super(PostListAPIView, self).get_queryset(*args, **kwargs)
-        queryset_list = Post.objects.all()
-        query = self.request.GET.get("q")
-        if query:
-            queryset_list = queryset_list.filter(
-                Q(title__icontains=query) |
-                Q(content__icontains=query) |
-                Q(user__first_name__icontains=query) |
-                Q(user__last_name__icontains=query)
-            ).distinct()
-        return queryset_list
+from .pagination import PostLimitOffsetPagination, PostPageNumberPagination
 
 
 class PostDetailAPIView(RetrieveAPIView):
@@ -66,3 +44,30 @@ class PostCreateAPIView(CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class PostListAPIView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
+
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'content', 'user__first_name']
+
+    # pagination
+    # pagination_class = PostLimitOffsetPagination (by  offset & limit)
+    pagination_class = PostPageNumberPagination
+
+    permission_classes = [AllowAny]
+
+    def get_queryset(self, *args, **kwargs):
+        # queryset_list = super(PostListAPIView, self).get_queryset(*args, **kwargs)
+        queryset_list = Post.objects.all()
+        query = self.request.GET.get("q")
+        if query:
+            queryset_list = queryset_list.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(user__first_name__icontains=query) |
+                Q(user__last_name__icontains=query)
+            ).distinct()
+        return queryset_list
